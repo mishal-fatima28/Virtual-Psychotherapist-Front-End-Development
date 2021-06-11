31e2f2cb0545a27fbd2ee5d351f2426869bab3a4
mishal-fatima28 <mishalfatima28@gmail.com>
2021-06-11 18:47:22 +0500

    first commit

---
+++ Virtualtherapist/js/main.js
@@ -0,0 +1,11 @@
+const inputs = document.querySelectorAll('.input');
+
+function focusFunc(){
+	let parent = this.parentNode.parentNode;
+	parent.classList.add('focus');
+}
+
+inputs.forEach(input => {
+
+	input.addEventListener('focus', focusFunc);
+});
